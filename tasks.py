b7223edb2b3da8b9805d08ef052de25798f1da13
melliphera <86661005+melliphera@users.noreply.github.com>
2021-12-01 10:10:49 +0000

    revised task1 function to operate off i and i+offset,  rather than iterate through /offset/ items and then compare i vs i-offset.

--- tasks.py
+++ tasks.py
@@ -2,19 +2,19 @@ def main():
     print(task1(offset=3))
 
 def task1(offset=1):  # offset=1 for task a, 3 for task b.
+    """iterates through task1 dataset, checks for all items if item is greater than the object
+    [offset] items earlier in the list."""
     counter = 0
     with open('data\\task1.txt', 'r') as f:
         data = f.readlines()
 
-    for i, strval in enumerate(data):
-        if i >= offset:
-            val = int(strval)
-            compval = int(data[i-offset])
-            if val > compval:
-                counter += 1
-            else:
-                print(f'Didn\'t increment for {val=}, {compval=}')
-            oldval = val
+    for i, strval in enumerate(data[:-offset]):
+        firstval = int(strval)
+        secondval = int(data[i+offset])
+        if secondval > firstval:
+            counter += 1
+        else:
+            print(f'Didn\'t increment for {firstval=}, {secondval=}')
 
     return counter
 
