51cd394abf45f84cae6d0179b044bf4690c289b3
melliphera <86661005+melliphera@users.noreply.github.com>
2021-12-01 10:05:49 +0000

    revise task 1a function to task1; enable handling of mutliple offsets.

--- tasks.py
+++ tasks.py
@@ -1,19 +1,20 @@
 def main():
-    print(task1a())
+    print(task1(offset=3))
 
-def task1a():
+def task1(offset=1):  # offset=1 for task a, 3 for task b.
     counter = 0
     with open('data\\task1.txt', 'r') as f:
         data = f.readlines()
 
-    oldval = int(data[0]) # set oldval to the first data so the first test returns 0 always
-    for strval in data:
-        val = int(strval) # take off newline, turn to number
-        if val > oldval:
-            counter += 1
-        else:
-            print(f'Didn\'t increment for {val=}, {oldval=}')
-        oldval = val
+    for i, strval in enumerate(data):
+        if i >= offset:
+            val = int(strval)
+            compval = int(data[i-offset])
+            if val > compval:
+                counter += 1
+            else:
+                print(f'Didn\'t increment for {val=}, {compval=}')
+            oldval = val
 
     return counter
 
